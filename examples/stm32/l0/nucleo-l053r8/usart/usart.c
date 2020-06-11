d500abe55e5cfb5f3840b5df9cd16a26cb3c2407
Rafael Lee <rafaellee.img@gmail.com>
2020-06-11 13:42:27 +0800

    Toggle LD2 and send char through usart2

--- examples/stm32/l0/nucleo-l053r8/usart/usart.c
+++ examples/stm32/l0/nucleo-l053r8/usart/usart.c
@@ -22,40 +22,65 @@
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/usart.h>
 
+
+#define LD2_PORT GPIOA
+#define LD2_PIN GPIO5
+
+#define CONSOLE_TX_PORT GPIOA
+#define CONSOLE_TX_PIN GPIO2
+#define CONSOLE_TX_AF GPIO_AF4
+
+#define CONSOLE_RX_PORT GPIOA
+#define CONSOLE_RX_PIN GPIO3
+#define CONSOLE_RX_AF GPIO_AF4
+
+#define CONSOLE_USART USART2
+
+
+
 static void clock_setup(void)
 {
 	/* Enable GPIOC clock for LED & USARTs. */
-	rcc_periph_clock_enable(RCC_GPIOC);
+	// rcc_periph_clock_enable(RCC_GPIOB);
 	rcc_periph_clock_enable(RCC_GPIOA);
 
 	/* Enable clocks for USART2. */
-	rcc_periph_clock_enable(RCC_USART1);
+	rcc_periph_clock_enable(RCC_USART2);
 }
 
 static void usart_setup(void)
 {
 	/* Setup USART2 parameters. */
-	usart_set_baudrate(USART1, 115200);
-	usart_set_databits(USART1, 8);
-	usart_set_parity(USART1, USART_PARITY_NONE);
-	usart_set_stopbits(USART1, USART_CR2_STOPBITS_1);
-	usart_set_mode(USART1, USART_MODE_TX);
-	usart_set_flow_control(USART1, USART_FLOWCONTROL_NONE);
+	usart_set_baudrate(USART2, 115200);
+	usart_set_databits(USART2, 8);
+	usart_set_parity(USART2, USART_PARITY_NONE);
+	usart_set_stopbits(USART2, USART_CR2_STOPBITS_1);
+	usart_set_mode(USART2, USART_MODE_TX);
+	usart_set_flow_control(USART2, USART_FLOWCONTROL_NONE);
 
 	/* Finally enable the USART. */
-	usart_enable(USART1);
+	usart_enable(USART2);
 }
 
 static void gpio_setup(void)
 {
 	/* Setup GPIO pin GPIO8/9 on GPIO port C for LEDs. */
-	gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO8 | GPIO9);
+	// gpio_mode_setup(GPIOB, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO13);
 
 	/* Setup GPIO pins for USART2 transmit. */
-	gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO9);
+	gpio_mode_setup(LD2_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LD2_PIN);
 
-	/* Setup USART1 TX pin as alternate function. */
-	gpio_set_af(GPIOA, GPIO_AF1, GPIO9);
+	// /* Setup USART2 TX pin as alternate function. */
+    // described in
+    // DM00104451 - PM0223 - Programming manual Cortex®-M0+ programming manual
+    // for STM32L0, STM32G0, STM32WL and STM32WB Series(Rev 13).pdf
+    // Table 16. Alternate function port A
+
+	gpio_mode_setup(CONSOLE_TX_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, CONSOLE_TX_PIN);
+	gpio_mode_setup(CONSOLE_RX_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, CONSOLE_RX_PIN);
+
+	gpio_set_af(CONSOLE_TX_PORT, CONSOLE_TX_AF, CONSOLE_TX_PIN);
+	gpio_set_af(CONSOLE_RX_PORT, CONSOLE_RX_AF, CONSOLE_RX_PIN);
 }
 
 int main(void)
@@ -68,12 +93,12 @@ int main(void)
 
 	/* Blink the LED (PD12) on the board with every transmitted byte. */
 	while (1) {
-		gpio_toggle(GPIOC, GPIO8);	/* LED on/off */
-		usart_send_blocking(USART1, c + '0'); /* USART1: Send byte. */
+		gpio_toggle(LD2_PORT, LD2_PIN);	/* LED on/off */
+		usart_send_blocking(USART2, c + '0'); /* USART2: Send byte. */
 		c = (c == 9) ? 0 : c + 1;	/* Increment c. */
 		if ((j++ % 80) == 0) {		/* Newline after line full. */
-			usart_send_blocking(USART1, '\r');
-			usart_send_blocking(USART1, '\n');
+			usart_send_blocking(USART2, '\r');
+			usart_send_blocking(USART2, '\n');
 		}
 		for (i = 0; i < 100000; i++) {	/* Wait a bit. */
 			__asm__("NOP");
